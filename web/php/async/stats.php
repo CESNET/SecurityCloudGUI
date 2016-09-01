955d7fce96afbaa42da28572641cf44e6d79583f
doomista <doomista@email.cz>
2016-09-01 15:05:20 +0200

    Statistics are fully working
    Statistics were rewritten using rrdtool. Statistics now work even for
    shadow profiles, they display separate channels, sum of channels and
    highlight most important channels.
    At this point the only part of the GUI dependant on the fdistdump is the
    querry part, rest is covered by RRDtool.

--- web/php/async/stats.php
+++ web/php/async/stats.php
@@ -1,12 +1,60 @@
 <?php
+/**
+ *  @brief Converts a unformatted number in a format with metric suffic
+ *  
+ *  @param [in] $rawNumber Anything, that can be considered a number (int, float, even number in a string)
+ *  @return String like: 134.1 M
+ *  
+ *  @details This function uses PHP native number_format(...) which does round number even though it is not documented
+ */
+function volume_conv_metric($rawNumber) {
+	$suffix = array("", "k", "M", "G", "T", "P", "E", "Z", "Y" );			// none, kilo, Mega, Giga, Tera, Peta, Exa, Zetta, Yotta
+	$suffixSize = (int)sizeof($suffix);
+	
+	$conv = floatval($rawNumber);
+	$ptr = 0;
+	
+	while ($conv > 1000.0 && $ptr < $suffixSize) {
+		$conv /= 1000.0;
+		$ptr++;
+	}
+	
+	$result = strval(number_format($conv, 1, '.', ' ')).' '.$suffix[$ptr];
+	
+	return $result;
+}
+
+/**
+ * prints the row of statistics
+ * first parameters is name of the row, will be printed in leftmost cell
+ * second parameter is array of statistics value, bgn and end are start/stop iterators in this array. they should have difference of five
+ * suffix is /s or B/s depending on situation
+ * highlight is true/false based on whether the row has the most significant data from all selected channels
+ */
+function printRow($rowName, $values, $bgn, $end, $suffix, $highlight) {
+	echo '<tr';
+	if ($highlight) {
+		echo ' class=\'danger\'';
+	}
+	echo '>';
+	echo '<th>', $rowName, '</th>';
+	
+	for ($iter = $bgn; $iter < $end; $iter++) {
+		echo '<td>', volume_conv_metric($values[$iter]), $suffix, '</td>';
+	}
+	
+	echo '</tr>';
+}
+
 // *** include ***
 include '../config.php';													// Grab some constants from the config
 include '../misc/profileClass.php';
 include '../misc/profileMethods.php';
 
 // *** get url params ***
-$time	= $_GET['time'];
+$time	= $_GET['time']; // < this parameter needs to be changed
 $src	= $_GET['src'];
+$timeSplit = explode(':', $time);
 
 /* CREATE A MASTER TREE FROM XML */
 $TREE_PROFILE = new Profile();												// Full tree of profiles
@@ -30,16 +78,15 @@ if ($aux == null) {
 	exit(2);
 }
 
-if ($aux->getShadow()) {
-	echo '<div class=\'panel panel-danger\'>';
-	echo '<div class=\'panel-heading\'>Shadow profile</div>';
-	echo '<div class=\'panel-body\'>This profile is a shadow profile, no metadata for statistics are available.</div>';
-	echo '</div>';
-	exit(3);
+if (sizeof($timeSplit) == 2) {
+	$time = "-s $timeSplit[0] -e $timeSplit[1]";
 }
+else {
+	$time = "-s $timeSplit[0] -e $timeSplit[0]";
+}
+$time = escapeshellcmd($time);
 
-// *** proc_open params ***
-$cmd = "exec $FDUMP $time --output-items=m --output-format=csv --output-volume-conv=metric-prefix $src | tail -n 3 | tr '\n' ','";	// <---------- $CMD --------------
+// *** PROC OPEN DESCRIPTORS ***
 $desc = array(
 	0 => array ('pipe', 'r'),
 	1 => array ('pipe', 'w'),
@@ -47,47 +94,84 @@ $desc = array(
 );
 $pipes = array();
 
-$p = proc_open($cmd, $desc, $pipes, $IPFIXCOL_DATA."$profile/");		// Execute the program command
-if($p == false) {														// If execution failed (
-	echo '<div class=\'panel panel-danger\'>';							// Print this *very* serious error
-	echo '<div class=\'panel-heading\'>Error</div>';
-	echo '<div class=\'panel-body\'>proc_exec() failed. Enable PHP error/warning logging to see what\'s the matter.</div>';
-	echo '</div>';
-	exit(4);															// And end this thread )
-}
+// *** DATA ARRAYS ***
+$stats = array();
+$total = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+$names = array("Flows", "Packets", "Traffic");
+$srcs = explode(':', $src);
+$srcsSize = (int)sizeof($srcs);
 
-$buffer = "";															// Stdout buffer
-if(is_resource($p)) {												
-	while($f = fgets($pipes[1])) {										// While any stdout is inbound
-		$buffer .= $f;													// Buffer it
+for ($s = 0; $s < $srcsSize; $s++) {
+	$cmd = "exec $RRDTOOL fetch $srcs[$s].rrd AVERAGE -r 300 $time -a | tail -n +3 | tr ' ' ',' | tr '\n' ';'";
+	
+	$p = proc_open($cmd, $desc, $pipes, $IPFIXCOL_DATA."$profile/rrd/channels");// Execute the program command
+	if($p == false) {														// If execution failed (
+		echo '<div class=\'panel panel-danger\'>';							// Print this *very* serious error
+		echo '<div class=\'panel-heading\'>Error</div>';
+		echo '<div class=\'panel-body\'>proc_exec() failed. Enable PHP error/warning logging to see what\'s the matter.</div>';
+		echo '</div>';
+		exit(4);															// And end this thread )
+	}
+
+	$buffer = "";															// Stdout buffer
+	if(is_resource($p)) {												
+		while($f = fgets($pipes[1])) {										// While any stdout is inbound
+			$buffer .= $f;													// Buffer it
+		}
+	}
+
+	$segments = explode(';', $buffer);
+	$segmentsSize = (int)sizeof($segments) - 1;
+
+	$stats[$s] = array();
+	for ($i = 0; $i < 15; $i++) {
+		$stats[$s][] = 0;
+	}
+	for ($i = 0; $i < $segmentsSize; $i++) {
+		$vars = explode(',', $segments[$i]);
+		$varsSize = (int)sizeof($vars);
+		
+		if ($varsSize != 20) {
+			echo "Unknown error occured.\n";
+			echo "$segments[$i]";
+			exit(666);
+		}
+		
+		for ($v = 1; $v <= 15; $v++) {
+			$stats[$s][$v-1] += floatval($vars[$v]);
+			$total[$v-1] += floatval($vars[$v]);
+		}
 	}
 }
 
-$ptime = explode('#', substr($time, 3));
-$stats = explode(',', $buffer);
-
 echo '<div class=\'panel panel-info\'>';
 echo '<div class=\'panel-heading\' id=\'StatsContentHeader\'></div></div>'; // content will be added by JS
-/*echo 'Statistics for: ';
-echo date('d M Y H:i',$ptime[0]);
-if (sizeof($ptime) > 1) {
-	echo ' - ',date('d M Y H:i',$ptime[1]);
-}
-echo '</div></div>';*/
 
+/* COMPUTE WHICH ROWS OF EACH SET (Flows, Packets, Traffic) has the most significant sum of values (All protos) */
+$maxima = array(0, 0, 0);
+for ($i = 0; $i < 3; $i++) {
+	for ($p = 0; $p < $srcsSize; $p++) {
+		if ($stats[$p][$i * 5] > $stats[$maxima[$i]][$i * 5]) $maxima[$i] = $p;
+	}
+}
+
+/* PRINT THE TABLES FOR FLOWS, PACKETS, TRAFFIC */
 echo '<div class=\'row\'>';
 for($i = 0; $i < 3; $i++) {
 	echo '<div class=\'col-md-4\'>';
 	echo '<div class=\'panel panel-info\'>';
-	echo '<div class=\'panel-heading\'>',$stats[$i * 6],'</div>';
+	echo '<div class=\'panel-heading\'>',$names[$i],'</div>';
 	echo '<div class=\'panel-body\'>';
-	echo '<table class=\'table table-striped table-condensed\'>';
-	echo '<thead><tr><th>All</th><th>TCP</th><th>UDP</th><th>ICMP</th><th>Other</th></thead>';
-	echo '<tbody><tr>';
-	for($p = 1; $p <=5; $p++) {
-		echo '<td>',$stats[$i * 6 + $p],'</td>';
+	echo '<table class=\'table table-striped table-condensed table-hover\'>';
+	echo '<thead><tr><th>Channel</th><th>All</th><th>TCP</th><th>UDP</th><th>ICMP</th><th>Other</th></thead>';
+	echo '<tbody>';
+	
+	for ($p = 0; $p < $srcsSize; $p++) {
+		printRow($srcs[$p], $stats[$p], $i * 5, ($i + 1) * 5, $i == 2 ? 'B/s' : '/s', $p == $maxima[$i]);
 	}
-	echo '</tr></tbody></table></div></div></div>';
+	printRow("Total", $total, $i * 5, ($i + 1) * 5, $i == 2 ? 'B/s' : '/s', false);
+	
+	echo '</tbody></table></div></div></div>';
 }
 echo '</div>';
 ?>
