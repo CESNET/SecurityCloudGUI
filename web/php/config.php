95c01433dd0ebdb6692920885edc3cfc7f929b60
doomista <doomista@email.cz>
2016-12-06 13:49:50 +0100

    slight change of implicit config

--- web/php/config.php
+++ web/php/config.php
@@ -11,7 +11,7 @@
 	$FDUMP				= 'mpiexec -n 2 fdistdump';
 	$FDUMP_HA			= 'fdistdump-ha';
 	$FDUMP_ENV			= array('PATH' => '/usr/lib64/mpich/bin:/usr/local/bin:/bin:/usr/local/sbin:/usr/bin:/usr/sbin');
-	$SINGLE_MACHINE		= false;	// Change this to true if you're running the whole SecurityCloud on the single machine. ($FDUMP will be called instead of $FDUMP_HA)
+	$SINGLE_MACHINE		= true;	// Change this to true if you're running the whole SecurityCloud on the single machine. ($FDUMP will be called instead of $FDUMP_HA)
 	
 	// RRD Tool
 	$RRDTOOL			= 'rrdtool';
